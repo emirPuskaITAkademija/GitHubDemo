c7260388f6468450a37da989698c4e81b6845e2c
Emir Puska <emir.puska@preventgroup.com>
2020-11-30 15:02:58 +0100

    Initial commit: Basic structure of project

---
+++ src/one/Circle.java
@@ -0,0 +1,14 @@
+package one;
+
+public class Circle extends Shape {
+
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/one/Shape.java
@@ -0,0 +1,16 @@
+package one;
+
+public abstract class Shape {
+
+    private int x;
+    private int y;
+
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public abstract void draw();
+
+}
